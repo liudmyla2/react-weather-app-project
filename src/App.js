b16099f85beb0dff97ad723d6a86f9f38558018a
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-11-25 13:37:11 +0200

    Added html, css, new components

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by Liudmyla Kostiuk and it is{" "}
-          <a href="https://github.com/liudmyla2/weather-app-react">
+          <a href="https://github.com/liudmyla2/react-weather-app-project">
             open sourced on GitHub
           </a>
         </footer>
